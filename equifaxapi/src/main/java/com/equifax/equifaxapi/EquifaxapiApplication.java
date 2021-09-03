2353d92641bdfabdd815b666648a7c6c3fb2d66d
Eder Coarita <Eder_Coarita@usmp.pe>
2021-09-03 16:28:49 -0500

    inicio de proyecto

---
+++ equifaxapi/src/main/java/com/equifax/equifaxapi/EquifaxapiApplication.java
@@ -0,0 +1,13 @@
+package com.equifax.equifaxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquifaxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquifaxapiApplication.class, args);
+	}
+
+}
---
+++ equifaxapi/src/test/java/com/equifax/equifaxapi/EquifaxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equifax.equifaxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquifaxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
