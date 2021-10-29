adb59229d0f1c767beb7b40231a14b68337aa726
Sandro Baldera <sandro_baldera@usmp.pe>
2021-10-29 13:31:27 -0500

    Registro

---
+++ web/src/main/java/com/equifaxweb/web/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package com.equifaxweb.web.repository;
+
+import com.equifaxweb.web.model.Cliente;
+import com.equifaxweb.web.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface  ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+
+    @Query(value = "SELECT o FROM Cliente o WHERE o.user=?1")
+    Cliente findByUsuario(Usuario user);
+    
+}
