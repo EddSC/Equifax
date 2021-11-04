12272ea1da7007fbe8a8ee7894a99d432e4a5941
Sandro Baldera <sandro_baldera@usmp.pe>
2021-11-04 00:49:03 -0500

    dniRegistro

--- web/src/main/java/com/equifaxweb/web/model/Cliente.java
+++ web/src/main/java/com/equifaxweb/web/model/Cliente.java
@@ -27,6 +27,7 @@ public class Cliente {
     private Integer id;
     private String firstName;
     private String lastName;
+    private String dni;
     private String email;
     private String phone;   
     @OneToOne(fetch = FetchType.LAZY)
